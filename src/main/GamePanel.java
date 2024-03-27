91b229f4831e00462b01590d131c40c5074ab3f3
LoreSam <lorenzano.samuele@darzo.net>
2024-03-27 14:04:44 +0100

    movimento + fps counter in console

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,7 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements Runnable{
 
@@ -15,12 +16,22 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //posizione di partenza del personaggio
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -28,8 +39,84 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
 
+/*     @Override
+   public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime()+drawInterval;
+        while (gameThread != null){
+
+            //update: aggiorniamo le informazioni del gioco(posizione personaggio)
+            update();
+
+            //draw: ridisegnamo la grafica
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+                if (remainingTime < 0)
+                {
+                    remainingTime=0;
+                }
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    primo metodo per fare gli fps
+    */
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long correntTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null){
+
+            correntTime = System.nanoTime();
+            delta +=(correntTime-lastTime)/drawInterval;
+            timer += (correntTime - lastTime);
+            lastTime = correntTime;
+            if (delta >= 1 ) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000)
+            {
+                System.out.println("FPS: "+drawCount);
+                drawCount=0;
+                timer=0;
+            }
+        }
+    }
+
+    public void update(){
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2=(Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX,playerY, tileSize, tileSize);
+        g2.dispose();
     }
 }
