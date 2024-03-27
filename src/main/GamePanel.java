dcc76dac8f3939ae7d8acba4ec991fd3c42a64f0
LoreSam <lorenzano.samuele@darzo.net>
2024-03-27 08:56:03 +0100

    creazione finestra base

---
+++ src/main/GamePanel.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    //IMPOSTAZIONI SCHERMO
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+}
