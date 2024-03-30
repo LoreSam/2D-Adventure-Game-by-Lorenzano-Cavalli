291d725847e9583474fb5e3bf70b2a6a3f84b87d
LoreSam <lorenzano.samuele@darzo.net>
2024-03-30 02:30:04 +0100

    aggiunta prima demo musica by myself

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -11,7 +11,7 @@ public class Sound {
     URL soundURL[] = new URL[30];
 
     public Sound() {
-        soundURL[0] = getClass().getResource("/sound/BlueBoyAdventure.wav");
+        soundURL[0] = getClass().getResource("/sound/demo1.wav");
         soundURL[1] = getClass().getResource("/sound/coin.wav");
         soundURL[2] = getClass().getResource("/sound/powerup.wav");
         soundURL[3] = getClass().getResource("/sound/unlock.wav");
